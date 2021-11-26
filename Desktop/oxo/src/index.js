25e2a3953b390f7073119395f41b2b390011adfc
Aboubacar Toure <aboubacar212toure@gmail.com>
2021-11-26 15:21:35 +0100

    affichage des vin avec les hooks usestate et useEffet

--- Desktop/oxo/src/index.js
+++ Desktop/oxo/src/index.js
@@ -1,61 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import axios from 'axios';
 const borderTable = {
   border: '2px solid red'
 }
 
+function FilterableProductTable() {
 
-class FilterableProductTable extends React.Component {
-  render() {
-    return (
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setData(result);
+        }
+
+      )
+  }, [])
+
+  return (
+    <div>
+      <input type="text" className="search-boxs" placeholder="search..." />
+      <input type="checkbox" />only show products
       <div>
-          <input type="text" className="search-boxs" placeholder="search..." />
-          <input type="checkbox" />only show products
-        <div>
-          <table style={borderTable}>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Price</th>
-              </tr>
-            </thead>
-            <tr><th colSpan="2">Sporting goods</th></tr>
+        <table style={borderTable}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
 
-            <tbody>
-              <tr>
-                <td>Footbal</td>
-                <td>$49.99</td>
-              </tr>
-              <tr>
-                <td>Baseball</td>
-                <td>$9.99</td>
-              </tr>
-              <tr>
-                <td>Basketball</td>
-                <td>$29.99</td>
-              </tr>
-            </tbody>
-            <tr><th colSpan="2">elctronics</th></tr>
-            <tbody>
-              <tr>
-                <td>IpadTouch</td>
-                <td>$239.99</td>
-              </tr>
-              <tr>
-                <td>Iphone 5</td>
-                <td>$129.99</td>
-              </tr>
-              <tr>
-                <td>Nexus</td>
-                <td>$29.99</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+          <tbody>
+            <tr><td>Catégorie</td></tr>
+            <tr>
+
+              <td >
+                <ul>
+                  {data.map(item => (
+                    <li key={item.id}> {item.name}</li>
+                  ))}
+                </ul>
+
+              </td>
+
+            </tr>
+          </tbody>
+        </table>
       </div>
-    )
-  }
-}
+    </div>
 
+  )
+
+}
+export default FilterableProductTable;
 ReactDOM.render(<FilterableProductTable />, document.getElementById('root'));
